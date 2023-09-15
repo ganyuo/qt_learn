7d97032209921ee92160b0836d59980cc3ae0cbf
ganyuo <ganyuo@outlook.com>
2023-09-15 17:46:06 +0800

    更新QPainter

--- projects/08_painter/src/main.cpp
+++ projects/08_painter/src/main.cpp
@@ -1,10 +1,30 @@
 #include <QApplication>
+#include <Qwidget>
+#include <QPainter>
+
+class my_painter : public QWidget
+{
+private:
+    /* data */
+public:
+    my_painter(QWidget *);
+    ~my_painter();
+
+    void paintEvent(QPaintEvent *e);
+};
+
+void my_painter::paintEvent(QPaintEvent *e)
+{
+    QPainter painter(this);
+
+    painter.
+}
 
 int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
 
-	event_filter widget;
+	my_painter widget;
 	widget.show();
 
 	return app.exec();
