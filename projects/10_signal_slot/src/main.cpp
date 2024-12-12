a979e603ea014eaa0518ee24dcb3903a51c5532a
ganyuo <ganyuo@outlook.com>
2024-12-12 11:23:01 +0800

    修改成多信号连接

--- projects/10_signal_slot/src/main.cpp
+++ projects/10_signal_slot/src/main.cpp
@@ -7,14 +7,25 @@ int main(int argc, char *argv[])
 {
 	QCoreApplication app(argc, argv);
 
-    my_signal sign;
+    my_signal sign_1, sign_2;
     my_slot slot;
 
-    /* 连接信号和槽, 信号函数的参数个数要大于槽函数 */
-    QObject::connect(&sign, SIGNAL(signal_fun(const char *, int)), &slot, SLOT(slot_fun(const char *)));
+    /* 两个信息号相连 */
+    QObject::connect(&sign_1, SIGNAL(signal_fun(const char *, int)),
+                     &sign_2, SIGNAL(signal_fun(const char *, int)));
+    /* 两个信号连接到同一个槽上 */
+    QObject::connect(&sign_1, SIGNAL(signal_fun(const char *, int)),
+                     &slot, SLOT(slot_fun(const char *)));
+    QObject::connect(&sign_2, SIGNAL(signal_fun(const char *, int)),
+                     &slot, SLOT(slot_fun(const char *)));
+    // QObject::disconnect(&sign_2, SIGNAL(signal_fun(const char *, int)),
+    //                     &slot, SLOT(slot_fun(const char *)));
 
-    /* 发送信号 */
-    emit sign.signal_fun("hello word", 1);
+    /* 发送信号1，信号2也会发送出去 */
+    emit sign_1.signal_fun("hello word 1", 1);
+
+    /* 发送信号2，信号1不会被发送出去 */
+    emit sign_2.signal_fun("hello word 2", 1);
 
 	return app.exec();
 }
